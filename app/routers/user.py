091a42183c6d70b5c52653982d47d3d07bcdc0ee
Jah-Wilson Teeba <teebawilson91@gmail.com>
2024-01-08 06:22:41 -0600

    Implemented email validation using AbstractAPI

--- app/routers/user.py
+++ app/routers/user.py
@@ -8,16 +8,24 @@ router = APIRouter(prefix="/users", tags=["Users"])
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.CreateUser, db: Session = Depends(get_db)):
-    # hash the password - user.password
     hashed_password = utils.hash(user.password)
     user.password = hashed_password
 
-    new_user = models.User(**user.dict())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
+    validate_email = utils.email_validation(user.email)
 
-    return new_user
+    if validate_email.get("deliverability") == "DELIVERABLE":
+        new_user = models.User(**user.model_dump())
+        print(new_user)
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+
+        return new_user
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"Email: {user.email} is not valid. Powered by AbstractAPI (https://www.abstractapi.com/)",
+        )
 
 
 @router.get("/{id}", response_model=schemas.UserOut)
